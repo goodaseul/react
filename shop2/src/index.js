a006c63737bc4fe6db4e6b14321ee6ff73eadd8b
goodaseul <gongdue3@gmail.com>
2024-06-09 22:31:01 +0900

    react 배포

--- shop2/src/index.js
+++ shop2/src/index.js
@@ -14,7 +14,7 @@ const root = ReactDOM.createRoot(document.getElementById("root"));
 root.render(
     <QueryClientProvider client={queryClient}>
         <Provider store={store}>
-            <BrowserRouter>
+            <BrowserRouter basename={process.env.PUBLIC_URL}>
                 <App />
             </BrowserRouter>
         </Provider>
