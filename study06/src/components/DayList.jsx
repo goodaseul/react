35f31eaab7b7a0e1c8cded50dc2083598ff53b42
goodaseul <gongdue3@gmail.com>
2024-07-25 12:20:55 +0900

    react study

--- study06/src/components/DayList.jsx
+++ study06/src/components/DayList.jsx
@@ -1,18 +1,31 @@
+import { useEffect, useState } from "react";
 import { Link } from "react-router-dom";
 
-import dummy from "../db/data.json";
-
 const DayList = () => {
+    const [days, setDays] = useState([]);
+
+    useEffect(() => {
+        fetch("http://localhost:3001/days")
+            .then((res) => {
+                return res.json();
+            })
+            .then((data) => {
+                setDays(data);
+            });
+    }, []);
+
     return (
-        <ul className="list_day">
-            {dummy.days.map((day) => {
-                return (
-                    <li key={day.id}>
-                        <Link to={`/day/${day.day}`}>Day {day.day}</Link>
-                    </li>
-                );
-            })}
-        </ul>
+        <>
+            <ul className="list_day">
+                {days.map((day) => {
+                    return (
+                        <li key={day.id}>
+                            <Link to={`/day/${day.day}`}>Day {day.day}</Link>
+                        </li>
+                    );
+                })}
+            </ul>
+        </>
     );
 };
 
