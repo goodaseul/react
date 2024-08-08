83a7d728506c46eb7ad18e3bbbf5bc45d9f6cc12
goodaseul <gongdue3@gmail.com>
2024-08-08 19:29:15 +0900

    react study

--- react-masterclass/src/index.tsx
+++ react-masterclass/src/index.tsx
@@ -3,14 +3,17 @@ import ReactDOM from "react-dom/client";
 import App from "./App";
 import { ThemeProvider } from "styled-components";
 import { theme } from "./theme";
+import { HelmetProvider } from "react-helmet-async";
 // import { RouterProvider } from "react-router-dom";
 // dark/light mode 처럼 theme할 시 프로퍼티들의 이름이 똑같아야함
 
 const root = ReactDOM.createRoot(document.getElementById("root") as HTMLElement);
 root.render(
     <React.StrictMode>
-        <ThemeProvider theme={theme}>
-            <App />
-        </ThemeProvider>
+        <HelmetProvider>
+            <ThemeProvider theme={theme}>
+                <App />
+            </ThemeProvider>
+        </HelmetProvider>
     </React.StrictMode>
 );
--- react-masterclass/src/theme.ts
+++ react-masterclass/src/theme.ts
@@ -1,9 +1,9 @@
 import { DefaultTheme } from "styled-components/dist/types";
 
 export const theme: DefaultTheme = {
-    bgColor: "white",
-    textColor: "black",
-    btnColor: "tomato",
+    bgColor: "#ced6e0",
+    textColor: "#2f3542",
+    accentColor: "#70a1ff",
 };
 // export const darkTheme: DefaultTheme = {
 //     bgColor: "black",
