813fa9e1165cf70d5c332dc2629a4b1e4c867da5
goodaseul <gongdue3@gmail.com>
2024-09-05 21:24:14 +0900

    react study

--- react-masterclass/src/routes/Coin.tsx
+++ react-masterclass/src/routes/Coin.tsx
@@ -1,8 +1,12 @@
 import { useParams } from "react-router-dom";
 
 const Coin = () => {
-    const { coinID } = useParams();
-    return <h1>Coin : {coinID}</h1>;
+    /* 
+        react-router-dom v6 이상 > 
+        useParams쓰는 순간 타입이 string or undefined로 됨.
+    */
+    const { coinId } = useParams();
+    return <h1>Coin : {coinId}</h1>;
 };
 
 export default Coin;
