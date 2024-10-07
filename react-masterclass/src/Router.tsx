7f636b542ae2c5eacc1edfbedea2000b1d1ac543
goodaseul <gongdue3@gmail.com>
2024-10-07 12:01:16 +0900

    react study

--- react-masterclass/src/Router.tsx
+++ react-masterclass/src/Router.tsx
@@ -1,13 +1,18 @@
 import { BrowserRouter, Routes, Route } from "react-router-dom";
 import Coins from "./routes/Main";
 import Coin from "./routes/Detail";
+import Chart from "./routes/Chart";
+import Price from "./routes/Price";
 
 function Router() {
     return (
         <BrowserRouter>
             <Routes>
-                <Route path="/:coinID" element={<Coin />} />
                 <Route path="/" element={<Coins />} />
+                <Route path="/:coinID" element={<Coin />}>
+                    <Route path="chart" element={<Chart />} />
+                    <Route path="price" element={<Price />} />
+                </Route>
             </Routes>
         </BrowserRouter>
     );
--- react-masterclass/src/routes/Detail.tsx
+++ react-masterclass/src/routes/Detail.tsx
@@ -1,6 +1,7 @@
 import { useState, useEffect } from "react";
-import { useParams, useLocation } from "react-router";
+import { Outlet, useParams, useLocation } from "react-router";
 import styled from "styled-components";
+
 const Container = styled.div`
     padding: 0 20px;
     max-width: 480px;
@@ -19,6 +20,29 @@ const Title = styled.h1`
     font-size: 48px;
     font-weight: bold;
 `;
+
+const Overview = styled.div`
+    display: flex;
+    justify-content: space-between;
+    background-color: rgba(0, 0, 0, 0.5);
+    padding: 10px 20px;
+    border-radius: 10px;
+`;
+const OverviewItem = styled.div`
+    display: flex;
+    flex-direction: column;
+    align-items: center;
+    span:first-child {
+        font-size: 10px;
+        font-weight: 400;
+        text-transform: uppercase;
+        margin-bottom: 5px;
+    }
+`;
+const Description = styled.p`
+    margin: 20px 0px;
+`;
+
 const Loader = styled.div`
     text-align: center;
 `;
@@ -108,11 +132,13 @@ const Coin = () => {
     useEffect(() => {
         (async () => {
             const infoData = await (await fetch(`https://api.coinpaprika.com/v1/coins/${coinID}`)).json();
-            setInfo(infoData);
             const priceData = await (await fetch(`https://api.coinpaprika.com/v1/tickers/${coinID}`)).json();
+            setInfo(infoData);
             setPriceInfo(priceData);
+            // 데이터 request 해준 후 loading false
+            setLoading(false);
         })();
-    }, []);
+    }, [coinID]);
 
     return (
         <Container>
@@ -123,9 +149,41 @@ const Coin = () => {
                     --------------------------------------------------------------
                     state.name 이 존재하면? name 을 가져오고  state가 존재하지 않을 때는 로딩 텍스트
                 */}
-                <Title>{state?.name || "Loading"}</Title>
+                <Title>{state?.name ? state.name : loading ? "Loading ..." : info?.name}</Title>
             </Header>
-            {loading ? <Loader>Loading....</Loader> : null}
+            {loading ? (
+                <Loader>Loading....</Loader>
+            ) : (
+                <>
+                    <Overview>
+                        <OverviewItem>
+                            <span>Rank:</span>
+                            <span>{info?.rank}</span>
+                        </OverviewItem>
+                        <OverviewItem>
+                            <span>Symbol:</span>
+                            <span>${info?.symbol}</span>
+                        </OverviewItem>
+                        <OverviewItem>
+                            <span>Open Source:</span>
+                            <span>{info?.open_source ? "Yes" : "No"}</span>
+                        </OverviewItem>
+                    </Overview>
+                    <Description>{info?.description}</Description>
+                    <Overview>
+                        <OverviewItem>
+                            <span>Total Suply:</span>
+                            <span>{priceInfo?.total_supply}</span>
+                        </OverviewItem>
+                        <OverviewItem>
+                            <span>Max Supply:</span>
+                            <span>{priceInfo?.max_supply}</span>
+                        </OverviewItem>
+                    </Overview>
+
+                    <Outlet />
+                </>
+            )}
         </Container>
     );
 };
