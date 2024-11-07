941c869815352f1d62e1ceb241f0fba0e434807f
goodaseul <gongdue3@gmail.com>
2024-11-07 20:22:09 +0900

    react study

---
+++ noma_todolist/src/theme.ts
@@ -0,0 +1,7 @@
+import { DefaultTheme } from "styled-components/dist/types";
+
+export const darkTheme: DefaultTheme = {
+    bgColor: "#ced6e0",
+    textColor: "black",
+    accentColor: "#70a1ff",
+};
