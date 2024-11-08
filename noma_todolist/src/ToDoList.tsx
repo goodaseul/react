c431eab9a7c480dffd47298c2201383f0838147f
goodaseul <gongdue3@gmail.com>
2024-11-08 17:28:37 +0900

    react study

--- noma_todolist/src/ToDoList.tsx
+++ noma_todolist/src/ToDoList.tsx
@@ -1,7 +1,30 @@
-import React from "react";
+import React, { useState } from "react";
 
 const ToDoList = () => {
-    return null;
+    const [toDo, setToDo] = useState("");
+    const [toDoError, setToDoError] = useState("");
+    const onChange = (event: React.FormEvent<HTMLInputElement>) => {
+        const {
+            currentTarget: { value },
+        } = event;
+        setToDo(value);
+    };
+    const onSubmit = (event: React.FormEvent<HTMLFormElement>) => {
+        event.preventDefault();
+        if (toDo.length < 10) {
+            return setToDoError("To do should be longer");
+        }
+        console.log("submit");
+    };
+    return (
+        <div>
+            <form onSubmit={onSubmit}>
+                <input onChange={onChange} value={toDo} type="text" placeholder="Write a to do" />
+                <button>Add</button>
+                {toDoError !== "" ? toDoError : null}
+            </form>
+        </div>
+    );
 };
 
 export default ToDoList;
