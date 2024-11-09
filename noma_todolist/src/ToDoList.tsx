a3a419bab7eb6d44b9f8daa30cb5efa8790771cc
goodaseul <gongdue3@gmail.com>
2024-11-09 17:41:41 +0900

    react study

--- noma_todolist/src/ToDoList.tsx
+++ noma_todolist/src/ToDoList.tsx
@@ -1,30 +1,52 @@
 import React, { useState } from "react";
+import { useForm } from "react-hook-form";
 
 const ToDoList = () => {
-    const [toDo, setToDo] = useState("");
-    const [toDoError, setToDoError] = useState("");
-    const onChange = (event: React.FormEvent<HTMLInputElement>) => {
-        const {
-            currentTarget: { value },
-        } = event;
-        setToDo(value);
-    };
-    const onSubmit = (event: React.FormEvent<HTMLFormElement>) => {
-        event.preventDefault();
-        if (toDo.length < 10) {
-            return setToDoError("To do should be longer");
-        }
-        console.log("submit");
-    };
+    const { register, watch } = useForm();
+    console.log(watch());
     return (
         <div>
-            <form onSubmit={onSubmit}>
-                <input onChange={onChange} value={toDo} type="text" placeholder="Write a to do" />
+            <form>
+                <input {...register("Email")} type="text" placeholder="Email" />
+                <input {...register("First Name")} type="text" placeholder="First Name" />
+                <input {...register("Last Name")} type="text" placeholder="Last Name" />
+                <input {...register("User Name")} type="text" placeholder="User Name" />
+                <input {...register("Password")} type="text" placeholder="Password" />
                 <button>Add</button>
-                {toDoError !== "" ? toDoError : null}
             </form>
         </div>
     );
 };
+//  *** register 사용으로 주석처리
+// const ToDoList = () => {
+//     const [toDo, setToDo] = useState("");
+//     const [toDoError, setToDoError] = useState("");
+
+//     const onChange = (event: React.FormEvent<HTMLInputElement>) => {
+//         const {
+//             currentTarget: { value },
+//         } = event;
+//         setToDoError("");
+//         setToDo(value);
+//     };
+//     const onSubmit = (event: React.FormEvent<HTMLFormElement>) => {
+//         event.preventDefault();
+//         if (toDo.length < 10) {
+//             return setToDoError("To do should be longer");
+//         }
+//         console.log("submit");
+//     };
+// *** register
+//     return (
+//         <div>
+//             <form onSubmit={onSubmit}>
+//                 <input onChange={onChange} value={toDo} type="text" placeholder="Write a to do" />
+//                 <button>Add</button>
+//                 {/* 빈 문자열이 아니라면 */}
+//                 {toDoError !== "" ? toDoError : null}
+//             </form>
+//         </div>
+//     );
+// };
 
 export default ToDoList;
