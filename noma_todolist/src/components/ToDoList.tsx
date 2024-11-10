de18a1be6797a70e074d29bd01b8f9e58da72e38
goodaseul <gongdue3@gmail.com>
2024-11-10 11:55:49 +0900

    react study

--- noma_todolist/src/components/ToDoList.tsx
+++ noma_todolist/src/components/ToDoList.tsx
@@ -1,49 +1,20 @@
 import React, { useState } from "react";
-import { useForm } from "react-hook-form";
-import { atom, useRecoilState, useRecoilValue, useSetRecoilState } from "recoil";
-
-interface IForm {
-    toDo: string;
-}
-interface IToDo {
-    id: number;
-    text: string;
-    category: "TO_DO" | "DOING" | "DONE";
-}
-
-const toDoState = atom<IToDo[]>({
-    key: "toDo",
-    default: [],
-});
+import { useRecoilValue } from "recoil";
+import CreateToDo from "./CreateToDo";
+import { toDoState } from "../atoms";
+import ToDo from "./ToDo";
 
 const ToDoList = () => {
-    const [toDo, setToDo] = useRecoilState(toDoState); //  <-- const value = useRecoilValue(toDoState); atom 으로 부터 값을 불러옴 const modFn = useSetRecoilState(toDoState);  atom 값을 바꿀 수 있음
+    const toDo = useRecoilValue(toDoState); //  <-- const value = useRecoilValue(toDoState); atom 으로 부터 값을 불러옴 const modFn = useSetRecoilState(toDoState);  atom 값을 바꿀 수 있음
 
-    const { register, handleSubmit, setValue } = useForm<IForm>();
-    const handleValid = ({ toDo }: IForm) => {
-        setToDo((oldToDo) => [{ text: toDo, id: Date.now(), category: "TO_DO" }, ...oldToDo]);
-        setValue("toDo", "");
-    };
-    console.log(toDo);
     return (
         <div>
             <h1>To Do</h1>
             <hr />
-            <form onSubmit={handleSubmit(handleValid)}>
-                <input
-                    {...register("toDo", {
-                        required: "Please write a To Do",
-                    })}
-                    type="text"
-                    placeholder="Write a to do"
-                />
-                <button>Add</button>
-            </form>
+            <CreateToDo />
             <ul>
                 {toDo.map((toDoList) => (
-                    <li key={toDoList.id}>
-                        {toDoList.category} {toDoList.text}{" "}
-                    </li>
+                    <ToDo key={toDoList.id} {...toDoList} />
                 ))}
             </ul>
         </div>
