63518b1be568cc483a859f709390db3a43aabf00
goodaseul <gongdue3@gmail.com>
2024-11-10 12:59:52 +0900

    react study

--- noma_todolist/src/components/ToDo.tsx
+++ noma_todolist/src/components/ToDo.tsx
@@ -1,12 +1,47 @@
 import React from "react";
-import { IToDo } from "../atoms";
-const ToDo = ({ text }: IToDo) => {
+import { IToDo, toDoState } from "../atoms";
+import { useSetRecoilState } from "recoil";
+const ToDo = ({ text, category, id }: IToDo) => {
+    // const onClick = (newCategory: IToDo["category"]) => {
+    //     console.log(newCategory);
+    // };
+
+    const setToDo = useSetRecoilState(toDoState);
+
+    const onClick = (event: React.MouseEvent<HTMLButtonElement>) => {
+        const {
+            currentTarget: { name },
+        } = event;
+
+        setToDo((oldToDo) => {
+            const targetIndex = oldToDo.findIndex((toDo) => toDo.id === id);
+            const oldToDoList = oldToDo[targetIndex];
+            const newToDoList = { text, id, category: name as any }; // 주석처리 한 익명함수가 더 나을듯
+            return [...oldToDo.slice(0, targetIndex), newToDoList, ...oldToDo.slice(targetIndex + 1)];
+        });
+    };
     return (
         <li>
             <span>{text}</span>
-            <button>To Do</button>
-            <button>Doing</button>
-            <button>Done</button>
+            {/* {category !== "TO_DO" && <button onClick={() => onClick("TO_DO")}>To Do</button>}
+            {category !== "DOING" && <button onClick={() => onClick("DOING")}>Doing</button>}
+            {category !== "DONE" && <button onClick={() => onClick("DONE")}>Done</button>} */}
+
+            {category !== "TO_DO" && (
+                <button name="TO_DO" onClick={onClick}>
+                    To Do
+                </button>
+            )}
+            {category !== "DOING" && (
+                <button name="DOING" onClick={onClick}>
+                    Doing
+                </button>
+            )}
+            {category !== "DONE" && (
+                <button name="DONE" onClick={onClick}>
+                    Done
+                </button>
+            )}
         </li>
     );
 };
--- noma_todolist/src/components/ToDoList.tsx
+++ noma_todolist/src/components/ToDoList.tsx
@@ -3,10 +3,10 @@ import { useRecoilValue } from "recoil";
 import CreateToDo from "./CreateToDo";
 import { toDoState } from "../atoms";
 import ToDo from "./ToDo";
+import { idText } from "typescript";
 
 const ToDoList = () => {
     const toDo = useRecoilValue(toDoState); //  <-- const value = useRecoilValue(toDoState); atom 으로 부터 값을 불러옴 const modFn = useSetRecoilState(toDoState);  atom 값을 바꿀 수 있음
-
     return (
         <div>
             <h1>To Do</h1>
