6db0e16a1ea9c3136127912375445735b010db82
goodaseul <gongdue3@gmail.com>
2024-11-11 19:22:35 +0900

    react study

--- noma_todolist/src/components/ToDoList.tsx
+++ noma_todolist/src/components/ToDoList.tsx
@@ -1,21 +1,36 @@
 import React from "react";
-import { useRecoilValue } from "recoil";
+import { useRecoilState, useRecoilValue } from "recoil";
 import CreateToDo from "./CreateToDo";
-import { toDoSelector, toDoState } from "../atoms";
+import { categoryState, toDoSelector } from "../atoms";
 import ToDo from "./ToDo";
 import { idText } from "typescript";
 
 const ToDoList = () => {
     // const toDo = useRecoilValue(toDoState); //  <-- const value = useRecoilValue(toDoState); atom 으로 부터 값을 불러옴 const modFn = useSetRecoilState(toDoState);  atom 값을 바꿀 수 있음
-    const [toDo, doing, done] = useRecoilValue(toDoSelector);
+    const toDos = useRecoilValue(toDoSelector);
+    const [category, setCategory] = useRecoilState(categoryState);
 
+    const onInput = (event: React.FormEvent<HTMLSelectElement>) => {
+        setCategory(event.currentTarget.value);
+    };
+
+    console.log(category);
     return (
         <div>
             <h1>To Do</h1>
             <hr />
+            <select value={category} onInput={onInput}>
+                <option value="TO_DO">To Do</option>
+                <option value="DOING">Doing</option>
+                <option value="DONE">Done</option>
+            </select>
             <CreateToDo />
+            {toDos?.map((toDo) => (
+                <ToDo key={toDo.id} {...toDo} />
+            ))}
+
             <h2>To Do</h2>
-            <ul>
+            {/* <ul>
                 {toDo.map((toDoList) => (
                     <ToDo key={toDoList.id} {...toDoList} />
                 ))}
@@ -33,7 +48,7 @@ const ToDoList = () => {
                 {done.map((toDoList) => (
                     <ToDo key={toDoList.id} {...toDoList} />
                 ))}
-            </ul>
+            </ul> */}
         </div>
     );
 };
