9c6cb2512885d1eaf97b2bdc5ec5b11c5f60d1fd
goodaseul <gongdue3@gmail.com>
2024-11-14 17:19:01 +0900

    react study

--- noma_trello_clone/src/components/Board.tsx
+++ noma_trello_clone/src/components/Board.tsx
@@ -1,13 +1,12 @@
 import { Droppable } from "@hello-pangea/dnd";
 // import { Droppable } from "react-beautiful-dnd";
-import React from "react";
+import React, { useRef } from "react";
 import styled from "styled-components";
 import DragabbleCard from "./DraggableCard";
 
 const Wrapper = styled.div`
     width: 300px;
-    padding: 20px 10px;
-    padding-top: 10px;
+    padding: 10px;
     background-color: ${(props) => props.theme.boardColor};
     border-radius: 5px;
     min-height: 300px;
@@ -29,9 +28,10 @@ interface IAreaProps {
 }
 
 const Area = styled.div<IAreaProps>`
-    background-color: ${(props) => (props.isDraggingOver ? "pink" : props.isDraggingFromThis ? "red" : "blue")};
+    background-color: ${(props) => (props.isDraggingOver ? "#dfe6e9" : props.isDraggingFromThis ? "#b2bec3" : "transparent")};
     flex-grow: 1;
     transition: background-color 0.3s ease-in-out;
+    padding: 20px;
 `;
 
 interface IBoardProps {
@@ -40,9 +40,18 @@ interface IBoardProps {
 }
 
 const Board = ({ toDos, boardId }: IBoardProps) => {
+    const inputRef = useRef<HTMLInputElement>(null);
+    const onClick = () => {
+        inputRef.current?.focus();
+        setTimeout(() => {
+            inputRef.current?.blur();
+        }, 5000);
+    };
     return (
         <Wrapper>
             <Title>{boardId}</Title>
+            <input ref={inputRef} placeholder="grab me" />
+            <button onClick={onClick}>click me</button>
             <Droppable droppableId={boardId}>
                 {(magic, info) => (
                     <Area isDraggingOver={info.isDraggingOver} isDraggingFromThis={Boolean(info.draggingFromThisWith)} ref={magic.innerRef} {...magic.droppableProps}>
--- noma_trello_clone/src/components/DraggableCard.tsx
+++ noma_trello_clone/src/components/DraggableCard.tsx
@@ -4,11 +4,12 @@ import { Draggable } from "@hello-pangea/dnd";
 import React from "react";
 import styled from "styled-components";
 
-const Card = styled.div`
+const Card = styled.div<{ isDragging: boolean }>`
     border-radius: 5px;
     margin-bottom: 5px;
     padding: 10px 10px;
-    background-color: ${(props) => props.theme.cardColor};
+    background-color: ${(props) => (props.isDragging ? "#7fb9ff" : props.theme.cardColor)};
+    box-shadow: ${(props) => (props.isDragging ? "0px 2px 25px rgba(0,0,0,.05)" : "none")};
 `;
 interface IDraggableCardProps {
     toDo: string;
@@ -19,8 +20,8 @@ const DraggableCard = ({ toDo, index }: IDraggableCardProps) => {
     console.log(toDo, "has been rendered");
     return (
         <Draggable key={toDo} draggableId={toDo} index={index}>
-            {(magic) => (
-                <Card ref={magic.innerRef} {...magic.dragHandleProps} {...magic.draggableProps}>
+            {(magic, info) => (
+                <Card isDragging={info.isDragging} ref={magic.innerRef} {...magic.dragHandleProps} {...magic.draggableProps}>
                     {toDo}
                 </Card>
             )}
