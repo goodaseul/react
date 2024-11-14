872c9ac613e886483632eb148f85c49e23d4ac23
goodaseul <gongdue3@gmail.com>
2024-11-14 18:16:54 +0900

    react study

--- noma_trello_clone/src/components/Board.tsx
+++ noma_trello_clone/src/components/Board.tsx
@@ -3,6 +3,7 @@ import { Droppable } from "@hello-pangea/dnd";
 import React, { useRef } from "react";
 import styled from "styled-components";
 import DragabbleCard from "./DraggableCard";
+import { useForm } from "react-hook-form";
 
 const Wrapper = styled.div`
     width: 300px;
@@ -34,13 +35,28 @@ const Area = styled.div<IAreaProps>`
     padding: 20px;
 `;
 
+const Form = styled.form`
+    width: 100%;
+    input {
+        width: 100%;
+    }
+`;
+
 interface IBoardProps {
     toDos: string[]; //string 이뤄진 배열
     boardId: string;
 }
 
+interface IForm {
+    toDo: string;
+}
+
 const Board = ({ toDos, boardId }: IBoardProps) => {
     const inputRef = useRef<HTMLInputElement>(null);
+    const { register, setValue, handleSubmit } = useForm<IForm>();
+    const onValid = (data: IForm) => {
+        setValue("toDo", "");
+    };
     const onClick = () => {
         inputRef.current?.focus();
         setTimeout(() => {
@@ -50,18 +66,20 @@ const Board = ({ toDos, boardId }: IBoardProps) => {
     return (
         <Wrapper>
             <Title>{boardId}</Title>
-            <input ref={inputRef} placeholder="grab me" />
-            <button onClick={onClick}>click me</button>
-            <Droppable droppableId={boardId}>
-                {(magic, info) => (
-                    <Area isDraggingOver={info.isDraggingOver} isDraggingFromThis={Boolean(info.draggingFromThisWith)} ref={magic.innerRef} {...magic.droppableProps}>
-                        {toDos.map((toDo, index) => (
-                            <DragabbleCard key={toDo} index={index} toDo={toDo} />
-                        ))}
-                        {magic.placeholder}
-                    </Area>
-                )}
-            </Droppable>
+            <Form onSubmit={handleSubmit(onValid)}>
+                <input {...register("toDo", { required: true })} type="text" placeholder={`Add task on ${boardId}`} />
+                <button onClick={onClick}>click me</button>
+                <Droppable droppableId={boardId}>
+                    {(magic, info) => (
+                        <Area isDraggingOver={info.isDraggingOver} isDraggingFromThis={Boolean(info.draggingFromThisWith)} ref={magic.innerRef} {...magic.droppableProps}>
+                            {toDos.map((toDo, index) => (
+                                <DragabbleCard key={toDo} index={index} toDo={toDo} />
+                            ))}
+                            {magic.placeholder}
+                        </Area>
+                    )}
+                </Droppable>
+            </Form>
         </Wrapper>
     );
 };
