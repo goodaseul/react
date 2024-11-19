3171c1a948d9ee124431b56ee0e4e7f82ee0f4cd
goodaseul <gongdue3@gmail.com>
2024-11-19 16:32:31 +0900

    react study

--- noma_ani/src/App.tsx
+++ noma_ani/src/App.tsx
@@ -9,18 +9,20 @@ const Wrapper = styled.div`
     align-items: center;
     height: 100vh;
 `;
-const Box = styled.div`
-    display: flex;
-    justify-content: center;
-    align-items: flex-start;
-    width: 100%;
-    gap: 10px;
+const Box = styled(motion.div)`
+    width: 200px;
+    height: 200px;
+    background-color: white;
+    border-radius: 15px;
+    box-shadow: 0 2px 3px rgba(0, 0, 0, 0.1), 0 10px 20px rgba(0, 0, 0, 0.06);
 `;
 
 function App() {
     return (
         <Wrapper>
-            <Box></Box>
+            <Box transition={{ type: "spring", delay: 1, bounce: 0.5 }} initial={{ scale: 0 }} animate={{ scale: 1, rotateZ: 360 }}>
+                &nbsp;
+            </Box>
         </Wrapper>
     );
 }
