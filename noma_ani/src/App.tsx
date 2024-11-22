62df4e14fa586adabd1c14016c279f994c1a858e
goodaseul <gongdue3@gmail.com>
2024-11-22 17:46:05 +0900

    react study

--- noma_ani/src/App.tsx
+++ noma_ani/src/App.tsx
@@ -1,14 +1,16 @@
 import styled from "styled-components";
-import { motion } from "framer-motion";
-import { useRef } from "react";
+import { motion, useMotionValue, useTransform, useViewportScroll } from "framer-motion";
+import { useEffect, useRef } from "react";
 
-const Wrapper = styled.div`
+const Wrapper = styled(motion.div)`
     display: flex;
+    flex-direction: column;
     width: 100vw;
     margin: 0 auto;
     justify-content: center;
     align-items: center;
-    height: 100vh;
+    height: 200vh;
+    background: linear-gradient(135deg, rgb(238, 0, 153), rgb(221, 0, 238));
 `;
 const Box = styled(motion.div)`
     width: 200px;
@@ -56,7 +58,6 @@ const circleVariants = {
 };
 
 const Box2 = styled(motion.div)`
-    margin-left: 20px;
     width: 200px;
     height: 200px;
     background-color: rgba(255, 255, 255, 0.2);
@@ -65,7 +66,13 @@ const Box2 = styled(motion.div)`
 `;
 
 const Box3 = styled(motion.div)`
-    margin-left: 20px;
+    width: 200px;
+    height: 200px;
+    background-color: rgba(255, 255, 255, 0.2);
+    border-radius: 40px;
+    box-shadow: 0 2px 3px rgba(0, 0, 0, 0.1), 0 10px 20px rgba(0, 0, 0, 0.06);
+`;
+const Box4 = styled(motion.div)`
     width: 200px;
     height: 200px;
     background-color: rgba(255, 255, 255, 0.2);
@@ -92,8 +99,21 @@ const boxVariants2 = {
 
 function App() {
     const biggerBoxRef = useRef<HTMLDivElement>(null);
+
+    const x = useMotionValue(0);
+    const rotateZ = useTransform(x, [-800, 800], [-360, 360]);
+    const gradient = useTransform(x, [-800, 800], ["linear-gradient(135deg,#6c8df1 0%,#272839 100%)", "linear-gradient(135deg,#9195a2 0%,#272839 100%)"]);
+    const { scrollYProgress } = useViewportScroll();
+    const scale = useTransform(scrollYProgress, [0, 1], [1, 5]);
+    useEffect(() => {
+        // x.onChange(() => console.log(x.get()));
+        // potato.onChange(() => console.log(potato.get()));
+        // scrollY.onChange(() => {
+        //     console.log(scrollY.get(), scrollYProgress.get());
+        // });
+    }, [scrollYProgress]);
     return (
-        <Wrapper>
+        <Wrapper style={{ background: gradient }}>
             {/* <Box transition={{ type: "spring", delay: 1, bounce: 0.5 }} initial={{ scale: 0 }} animate={{ scale: 1, rotateZ: 360 }}> 기본 애니메이션
              */}
             {/* <Box variants={myVars} initial="start" animate="end"> variants 를 사용한 애니메이션 */}
@@ -103,12 +123,13 @@ function App() {
                 <Circle variants={circleVariants} />
                 <Circle variants={circleVariants} />
             </Box>
-
             <Box2 variants={boxVariants2} whileHover="hover" whileTap="click" />
-
             <BiggerBox ref={biggerBoxRef}>
                 <Box3 drag dragConstraints={biggerBoxRef} dragSnapToOrigin dragElastic={0.5} variants={boxVariants2} whileDrag="drag" />
             </BiggerBox>
+
+            {/* <button onClick={() => x.set(200)}> Click me </button> */}
+            <Box4 style={{ x, rotateZ, scale }} drag="x" dragSnapToOrigin />
         </Wrapper>
     );
 }
