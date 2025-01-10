7fe76c33916a84b23cd1ba0bf1fce296a59125aa
goodaseul <gongdue3@gmail.com>
2025-01-10 11:36:31 +0900

    todolist 스타일 완성

--- 2025/todolist/src/components/TodoList.tsx
+++ 2025/todolist/src/components/TodoList.tsx
@@ -22,20 +22,24 @@ const TodoList: React.FC<TodoListProps> = ({ removeTodo, toggleComplete, saveTod
                     {editingTodoId === todo.id ? (
                         <>
                             <input type="text" value={editingText} onChange={(e) => setEditingText(e.target.value)} />
-                            <button onClick={saveTodo}>저장</button>
-                            {/* 수정 모드 종료 */}
-                            <button onClick={() => setEditingTodoId(null)}>취소</button>
+                            <div className="wrap_edit">
+                                <button onClick={saveTodo}>저장</button>
+                                {/* 수정 모드 종료 */}
+                                <button onClick={() => setEditingTodoId(null)}>취소</button>
+                            </div>
                         </>
                     ) : (
                         // null일 때
                         <>
-                            <p style={{ textDecoration: todo.completed ? "line-through" : "none" }}>{todo.text}</p>
+                            <p style={{ textDecoration: todo.completed ? "line-through #e2725b" : "none" }}>{todo.text}</p>
                             {/* 수정 버튼을 누를시  
                                 수정 버튼을 누른 Todo의 ID와 기존 텍스트를 받아와서
                             */}
-                            <button onClick={() => startEditing(todo.id, todo.text)}> 수정 </button>
-                            <button onClick={() => toggleComplete(todo.id)}> 완료 </button>
-                            <button onClick={() => removeTodo(todo.id)}> Remove</button>
+                            <div className="wrap_edit">
+                                <button onClick={() => startEditing(todo.id, todo.text)}> 수정 </button>
+                                <button onClick={() => toggleComplete(todo.id)}> 완료 </button>
+                                <button onClick={() => removeTodo(todo.id)}> Remove</button>
+                            </div>
                         </>
                     )}
                 </li>
